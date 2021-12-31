b5d86f5b523e7a14346f6f4f43010689cbaef719
Rubén de Diego <rubendediegogomez@gmail.com>
2021-12-31 10:29:44 +0100

    create base button

---
+++ src/components/BaseComponents/BaseButton.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { defineProps,Prototype } from 'vue';
+
+defineProps({
+  text: {
+    type: String as Prototype<string>,
+    required:true
+  }
+})
+
+</script>
+
+<template>
+     <button>{{ text }}</button>
+</template>
+
+<style scoped>
+
+</style>
